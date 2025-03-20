14c9e8fe2bdfe66d3cf47cc2a77b8549f6576bab
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-03-20 15:40:24 +0530

    added counter app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
 import React from "react";
-import './../styles/App.css';
+import { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  let [counter, setCounter] = useState(0);
+
+  const add = () => {
+    counter = counter + 1;
+    setCounter(counter);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      <p>Button Clicked {counter} times</p>
+      <button onClick={add}>Click me</button>
+    </div>
+  );
+};
+
+export default App;
